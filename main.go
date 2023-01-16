421174fe3a3b488cac770ae398438a0b974b65aa
Burak Yeşilyurt <burakyesilyurt@duck.com>
2023-01-17 00:29:18 +0300

    added comments

--- main.go
+++ main.go
@@ -11,8 +11,9 @@ const ipAddress = "127.0.0.1"
 
 var url = fmt.Sprintf("%s:%s", ipAddress, portNumber)
 
+// Serves home page by running this function whenever <url>/ is requested.
 func HomeHandler(w http.ResponseWriter, r *http.Request) {
-	nb, err := fmt.Fprintf(w, "Hello Web")
+	nb, err := fmt.Fprintf(w, "Hello Web") // writes to w (we send browser the data via `w` response writer)
 	if err != nil {
 		log.Fatal(err)
 	}
@@ -25,6 +26,6 @@ func main() {
 
 	// listening http requests
 	fmt.Printf("Listening traffic at %s\n", url)
-	err := http.ListenAndServe(url, nil)
+	err := http.ListenAndServe(url, nil) // 2nd parameter is `nil` since we didn't send any information to the page.
 	log.Fatal(err)
 }
