14b7a8f821f3ac909f72d6ced0db7429f488acf6
Burak Yeşilyurt <burakyesilyurt@duck.com>
2023-01-17 00:04:10 +0300

    added basic web functions

--- main.go
+++ main.go
@@ -1,5 +1,30 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"log"
+	"net/http"
+)
 
+const portNumber = "8080"
+const ipAddress = "127.0.0.1"
+
+var url = fmt.Sprintf("%s:%s", ipAddress, portNumber)
+
+func HomeHandler(w http.ResponseWriter, r *http.Request) {
+	nb, err := fmt.Fprintf(w, "Hello Web")
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Printf("Number of bytes written: %d\n", nb)
+}
+
+func main() {
+	// routing
+	http.HandleFunc("/", HomeHandler)
+
+	// listening http requests
+	fmt.Printf("Listening traffic at %s\n", url)
+	err := http.ListenAndServe(url, nil)
+	log.Fatal(err)
 }
